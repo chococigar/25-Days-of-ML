d551c02663c69c306de83fb5a83ed77d4ba08139
Jiyoun <jiyounjenniferha@gmail.com>
2018-09-06 22:18:54 +0900

    day 1

---
+++ Day1_markov_generator/markov_generator.py
@@ -0,0 +1,60 @@
+"""
+Modified from original code @ https://github.com/llSourcell/Rap_Lyric_Generator/blob/master/MarkovRap.py
+"""
+
+import random, re, os
+
+# freqDict is a dict of dict containing frequencies
+def addToDict(fileName, freqDict):
+	f = open(fileName, 'r')
+	words = re.sub("\n", " \n", f.read()).lower().split(' ')
+
+	# count frequencies curr -> succ
+	for curr, succ in zip(words[1:], words[:-1]):
+		# check if curr is already in the dict of dicts
+		if curr not in freqDict:
+			freqDict[curr] = {succ: 1}
+		else:
+			# check if the dict associated with curr already has succ
+			if succ not in freqDict[curr]:
+				freqDict[curr][succ] = 1;
+			else:
+				freqDict[curr][succ] += 1;
+
+	# compute percentages
+	probDict = {}
+	for curr, currDict in freqDict.items():
+		probDict[curr] = {}
+		currTotal = sum(currDict.values())
+		for succ in currDict:
+			probDict[curr][succ] = currDict[succ] / currTotal
+	return probDict
+
+def markov_next(curr, probDict):
+	if curr not in probDict:
+		return random.choice(list(probDict.keys()))
+	else:
+		succProbs = probDict[curr]
+		randProb = random.random()
+		currProb = 0.0
+		for succ in succProbs:
+			currProb += succProbs[succ]
+			if randProb <= currProb:
+				return succ
+		return random.choice(list(probDict.keys()))
+
+def generateLyrics(curr, probDict, T = 100):
+	lyrics = [curr]
+	for t in range(T):
+		lyrics.append(markov_next(lyrics[-1], probDict))
+	return " ".join(lyrics)
+
+if __name__ == '__main__':
+	lyricsFreqDict = {}
+	lyrics_dir = os.environ['OLDPWD']+'/lyrics'
+	for lyrics in os.listdir(lyrics_dir):
+		lyricsProbDict = addToDict(lyrics_dir+"/"+lyrics, lyricsFreqDict)
+
+	startWord = input("What do you want to start your lyrics with?\n > ")
+	print("Alright, here's your lyrics:")
+	print(generateLyrics(startWord, lyricsProbDict))
--- log.md
+++ log.md
@@ -2,7 +2,8 @@
 
 ## Day 0 : September 6th, 2018
  
-**Today's Progress** : 
+**Today's Progress** : Attempted Siraj's Hidden Markov model lyrics generator, just considering frequency (not grammar / semantics)
 
-**Thoughts** : 
+**Thoughts** : Multi-lingual training is difficult, as they have different rules (especially Korean, where syllables are composed of 3 consonants and vowels). Might need to attempt Word2Vec to perform better.
 
+**Link of Work:**   [Commit]()
